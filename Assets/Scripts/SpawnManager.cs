880a9a6f2f19047819da746fc8fc7f85e6ac9c10
David Carmona - Contractor <David.Carmona@contractor.qiagen.com>
2022-10-27 17:51:19 +0200

    add powerup spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,15 +7,20 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
+    private GameObject _powerupPrefab;
+    [SerializeField]
     private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _powerupContainer;
     [SerializeField]
     private bool isPlayerAlive = true;
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
-    IEnumerator SpawnRoutine ()
+    IEnumerator SpawnEnemyRoutine ()
     {
         while (isPlayerAlive)
         {
@@ -23,6 +28,17 @@ public class SpawnManager : MonoBehaviour
             GameObject newEnemy = Instantiate<GameObject>(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerupRoutine ()
+    {
+        while (isPlayerAlive)
+        {
+            Vector3 posToSpawnPowerup = new Vector3(Random.Range(-9.4f, 9.4f), 6.4f, 0);
+            GameObject newPowerup = Instantiate<GameObject>(_powerupPrefab, posToSpawnPowerup, Quaternion.identity);
+            newPowerup.transform.parent = _powerupContainer.transform;
+            yield return new WaitForSeconds(Random.Range(32f, 7f));
         }
     }
 
